84ce0af5479968c13b9628d10189fbdec9e8c759
Unknown <kaa516@nyu.edu>
2018-02-02 14:13:22 -0500

    Initial commit w/ project

---
+++ Assets/Scripts/EndWallCollide.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndWallCollide : MonoBehaviour {
+
+	/* void OnTriggerEnter (Collider col) {
+		if (col.gameObject.name == "player")
+			SceneManager.LoadScene (1);
+		Debug.Log ("collide");
+			} */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/*private void OnCollisionEnter2D (Collision2D coll){
+		//Debug.Log (_col.name);
+
+		if(coll.name == "player"){
+			SceneManager.LoadScene(1);
+		}
+	}*/
+}
---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.AccessControl;
+using UnityEngine.Experimental.UIElements.StyleEnums;
+using UnityEngine.SceneManagement;
+
+public class playerMove : MonoBehaviour {
+	
+	public float moveSpeed = 0.3f;
+
+	public KeyCode rightKey = KeyCode.D;
+	public KeyCode leftKey = KeyCode.A;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (rightKey)){
+			transform.Translate (moveSpeed, 0, 0);
+			GetComponent<SpriteRenderer> ().flipX = false;
+		}
+			if(Input.GetKey (leftKey)){
+				transform.Translate (-moveSpeed, 0, 0);
+			GetComponent<SpriteRenderer>().flipX = true;
+			}
+
+		if (transform.position.x > 115) {
+			SceneManager.LoadScene (1);
+		}
+
+	}
+}
